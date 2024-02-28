c20b0e21d2c948dbf9b024181f27c3cade47dc8d
HashimBabinu <hashimbabnu272@gmail.com>
2024-02-28 16:41:15 +0530

    6th commit

--- lib/AdminComplaints.dart
+++ lib/AdminComplaints.dart
@@ -14,6 +14,7 @@ class _AdminComplaintState extends State<AdminComplaint> {
       body: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 30),
@@ -33,20 +34,47 @@ class _AdminComplaintState extends State<AdminComplaint> {
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 80),
+                child: Container(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 45,top: 7),
+                    child: Text(
+                      'Driver',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, color: Colors.black),
+                    ),
+                  ),
+                  width: 123,
+                  height: 34,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF3DE07E),
+                      border: Border.all(color: Colors.black, width: 1),
+                      borderRadius: BorderRadius.circular(10)),
+                
+                ),
+              ),
               Container(
-                child: Text(
-                  'Driver',
-                  style: TextStyle(
-                      fontWeight: FontWeight.w500, color: Colors.black),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 22,top: 7),
+                  child: Text(
+                    'Recycle Team',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500, color: Colors.black),
+                  ),
                 ),
                 width: 123,
                 height: 34,
                 decoration: BoxDecoration(
                     color: Color(0xFF3DE07E),
-                    border: Border.all(color: Colors.black, width: 1)),
+                    border: Border.all(color: Colors.black, width: 1),
+                    borderRadius: BorderRadius.circular(10)),
                     
+
               ),
+
             ],
           )
         ],
